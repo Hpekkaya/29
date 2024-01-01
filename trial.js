bbca15b9b855ea6b05328bea80b4440768410eaa
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-01 23:25:18 +0300

    Post request with async

--- trial.js
+++ trial.js
@@ -54,22 +54,65 @@
 // .catch(err => console.log(err))
 
 // Get request with async 
-class Request {
-        async get(url){
-            const response = await fetch(url)
-            const data = response.json()
-            return data            
-        }
+// class Request {
+//         async get(url){
+//             const response = await fetch(url)
+//             const data = response.json()
+//             return data            
+//         }
+// }
+
+// const request = new Request();
+// let albums;
+// request.get("https://jsonplaceholder.typicode.com/albums")
+// .then(data => {
+//     albums =data;
+//     console.log(albums)    
+// })
+// .catch(err => console.log(err))
+
+//  Post request
+// class Request {
+//     post(url,data){
+//         return new Promise((resolve,reject) =>{
+//             fetch(url, {
+//                 method: "POST",
+//                 body: JSON.stringify(data),
+//                 headers: {
+//                     "Content-type": "application/json; charset=UTF-8"
+//                 }
+//             })
+//             .then(response => response.json())
+//             .then(data => resolve(data))
+//             .catch(err => reject(err))
+//         })
+//     }
+// }
+// const request = new Request();
+// request.post("https://jsonplaceholder.typicode.com/albums",{userId:1,title:"Thriller"})
+// .then(newAlbum => console.log(newAlbum))
+// .catch(err => console.log(err))
+
+// Post request with async
+class Request{
+    async post(url,data){
+        const response = await fetch(url, {
+            method : "POST",
+            body :JSON.stringify(data),
+            headers :{
+                "Content-type" : "application/json; charset=UTF-8"
+            }
+        })
+        const responseData = await response.json()
+        return responseData
+    }
 }
 
-const request = new Request();
-let albums;
-request.get("https://jsonplaceholder.typicode.com/albums")
-.then(data => {
-    albums =data;
-    console.log(albums)    
-})
+const request = new Request()
+request.post("https://jsonplaceholder.typicode.com/albums",{userId:2,name:"Hakan"})
+.then(dataResponse => console.log(dataResponse))
 .catch(err => console.log(err))
+
 
 
 
