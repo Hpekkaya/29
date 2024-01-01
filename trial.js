084b67d89c97e4815b67b183e75d902e0fbb0ed7
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-01 23:59:17 +0300

    Put request (updating) with async

--- trial.js
+++ trial.js
@@ -114,26 +114,44 @@
 // .catch(err => console.log(err))
 
 // Put request --- Updating the current data
-class Request {
-    put(url,data){
-        return new Promise((resolve,reject) =>{
-            fetch(url, {
-                method: "PUT",
-                body: JSON.stringify(data),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            })
-            .then(response => response.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err))
+// class Request {
+//     put(url,data){
+//         return new Promise((resolve,reject) =>{
+//             fetch(url, {
+//                 method: "PUT",
+//                 body: JSON.stringify(data),
+//                 headers: {
+//                     "Content-type": "application/json; charset=UTF-8"
+//                 }
+//             })
+//             .then(response => response.json())
+//             .then(data => resolve(data))
+//             .catch(err => reject(err))
+//         })
+//     }
+// }
+// const request = new Request();
+// request.put("https://jsonplaceholder.typicode.com/albums/8",{userId:5,title:"Thriller"})
+// .then(newAlbum => console.log(newAlbum))
+// .catch(err => console.log(err))
+
+// Put request (updating) with async
+class Request{
+    async put(url,data){
+        const response = await fetch(url, {
+            method : "PUT",
+            body :JSON.stringify(data),
+            headers :{
+                "Content-type" : "application/json; charset=UTF-8"
+            }
         })
+        const responseData = await response.json()
+        return responseData
     }
 }
-const request = new Request();
-request.put("https://jsonplaceholder.typicode.com/albums/8",{userId:5,title:"Thriller"})
-.then(newAlbum => console.log(newAlbum))
+
+const request = new Request()
+request.put("https://jsonplaceholder.typicode.com/albums/8",{userId:2,name:"Thriller"})
+.then(dataResponse => console.log(dataResponse))
 .catch(err => console.log(err))
-
-
 
