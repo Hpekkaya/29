9b9774c1767d2a939e385d3b74a87be6eb8617bf
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-01 21:37:14 +0300

    class request

--- trial.js
+++ trial.js
@@ -1,19 +1,46 @@
 // An application programming interface (API) 
 // is a way for two or more computer programs to communicate with each other. It is a type of software interface,
 
-function fetchResponse (){
-    fetch("ajax.txt")
-    .then(response => response.text())
-    .then(text => console.log(text))
-    .catch(err => console.log(err))
-}
-fetchResponse()
+// function fetchResponse (){
+//     fetch("ajax.txt")
+//     .then(response => response.text())
+//     .then(text => console.log(text))
+//     .catch(err => console.log(err))
+// }
+// fetchResponse()
 
-// getting from JSON
-getJasonFile =() =>{
-    fetch("students.json")
-    .then(response => response.json())
-    .then(Jtext => console.log(Jtext))
-    .catch(err => console.log(err))
+// // getting from JSON
+// getJasonFile =() =>{
+//     fetch("students.json")
+//     .then(response => response.json())
+//     .then(Jtext => console.log(Jtext))
+//     .catch(err => console.log(err))
+// }
+// getJasonFile()
+
+// Get request all the data
+
+// class Request {
+//     get(url){
+//         fetch(url)
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(err => console.log(err))
+//     }
+// }
+// const request =new Request()
+// request.get("https://jsonplaceholder.typicode.com/albums")
+
+
+// Get request processed the data
+
+class Request {
+    get(url){
+        fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }
 }
-getJasonFile()
+const request =new Request()
+request.get("https://jsonplaceholder.typicode.com/albums")
