67dbf616a0727e29eb3007dcf03850b71a32f48b
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-02 00:18:38 +0300

    delete request

--- trial.js
+++ trial.js
@@ -114,6 +114,10 @@
 // .catch(err => console.log(err))
 
 // Put request --- Updating the current data
+// Delete the existing content completely and update the new data- 
+// In order to update data we use put 
+// If we aim to some addings and save previous data we use patch 
+
 // class Request {
 //     put(url,data){
 //         return new Promise((resolve,reject) =>{
@@ -136,22 +140,37 @@
 // .catch(err => console.log(err))
 
 // Put request (updating) with async
-class Request{
-    async put(url,data){
-        const response = await fetch(url, {
-            method : "PUT",
-            body :JSON.stringify(data),
-            headers :{
-                "Content-type" : "application/json; charset=UTF-8"
-            }
+// class Request{
+//     async put(url,data){
+//         const response = await fetch(url, {
+//             method : "PUT",
+//             body :JSON.stringify(data),
+//             headers :{
+//                 "Content-type" : "application/json; charset=UTF-8"
+//             }
+//         })
+//         const responseData = await response.json()
+//         return responseData
+//     }
+// }
+
+// const request = new Request()
+// request.put("https://jsonplaceholder.typicode.com/albums/8",{userId:2,name:"Thriller"})
+// .then(dataResponse => console.log(dataResponse))
+// .catch(err => console.log(err))
+
+//  delete request
+class Request {
+    delete(url){
+        return new Promise((resolve,reject) =>{
+            fetch(url, {method: "DELETE"})
+            .then(() => resolve("Deleting Process is succesful") )
+            .catch(err => reject(err))
         })
-        const responseData = await response.json()
-        return responseData
     }
 }
 
-const request = new Request()
-request.put("https://jsonplaceholder.typicode.com/albums/8",{userId:2,name:"Thriller"})
-.then(dataResponse => console.log(dataResponse))
+const request = new Request();
+request.delete("https://jsonplaceholder.typicode.com/albums/8",{userId:5,title:"Thriller"})
+.then(message => console.log(message))
 .catch(err => console.log(err))
-
